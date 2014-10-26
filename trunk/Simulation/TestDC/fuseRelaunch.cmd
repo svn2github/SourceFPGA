73ba1cdb7d19e79f3bf8905811f0b5ce28be0406
mimey64@gmail.com <mimey64@gmail.com@97322833-781f-d049-4fa6-41445d72aac9>
2014-10-26 06:15:35 +0000

    Will be replaced by TestDec
    git-svn-id: http://batdroid.googlecode.com/svn/SourceFPGA@123 97322833-781f-d049-4fa6-41445d72aac9

--- trunk/Simulation/TestDC/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "secureip" -o "E:/Source/BatDroidV2/SourceFPGA/Simulation/TestDC/DCTestbench_isim_beh.exe" -prj "E:/Source/BatDroidV2/SourceFPGA/Simulation/TestDC/DCTestbench_beh.prj" "work.DCTestbench" 
